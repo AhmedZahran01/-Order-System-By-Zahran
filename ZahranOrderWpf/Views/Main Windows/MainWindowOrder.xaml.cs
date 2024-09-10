f79874a2a316269e8f24875b2425a6f660cc6d81
Ahmed Saeed <intern_dev@dewansoft.com>
2024-09-10 17:32:12 +0300

    asds

---
+++ ZahranOrderWpf/Views/Main Windows/MainWindowOrder.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ZahranOrderWpf.Views.Main_Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindowOrder.xaml
+    /// </summary>
+    public partial class MainWindowOrder : Window
+    {
+        public MainWindowOrder()
+        {
+            InitializeComponent();
+        }
+
+        private void AddTransactionBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void IconHome_Clic(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CloseShiftBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
